06b55a8e0abcc735eedab58eea46a4ba6b5c9e33
ljmccode <lmcsay@gmail.com>
2023-08-22 09:36:37 -0500

    Create Order Services. Closes #2.

---
+++ src/main/java/Ahead/JSONDataProcessing/services/OrderService.java
@@ -0,0 +1,9 @@
+package Ahead.JSONDataProcessing.services;
+
+import Ahead.JSONDataProcessing.model.OrderData;
+import Ahead.JSONDataProcessing.model.OrderInfo;
+
+public interface OrderService {
+
+    OrderInfo evaluateOrders(OrderData orderData);
+}
---
+++ src/main/java/Ahead/JSONDataProcessing/services/OrderServiceImpl.java
@@ -0,0 +1,13 @@
+package Ahead.JSONDataProcessing.services;
+
+import Ahead.JSONDataProcessing.model.OrderData;
+import Ahead.JSONDataProcessing.model.OrderInfo;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+    @Override
+    public OrderInfo evaluateOrders(OrderData orderData) {
+        return null;
+    }
+}
