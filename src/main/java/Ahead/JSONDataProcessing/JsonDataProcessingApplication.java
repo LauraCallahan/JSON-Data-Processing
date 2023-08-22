914f7bec6d0c7a19e6d8074e64b03d38b0f112f6
ljmccode <lmcsay@gmail.com>
2023-08-22 09:16:18 -0500

    Initial Commit

---
+++ src/main/java/Ahead/JSONDataProcessing/JsonDataProcessingApplication.java
@@ -0,0 +1,13 @@
+package Ahead.JSONDataProcessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonDataProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonDataProcessingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Ahead/JSONDataProcessing/JsonDataProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package Ahead.JSONDataProcessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonDataProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
